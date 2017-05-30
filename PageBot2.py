0e8ee5522b709a5ec2ca09696a1a8268bb88266c
James Cullen <jimcullenaus@gmail.com>
2017-05-31 05:14:43 +1000

    Major changes made to adapt to PRAW4

--- PageBot2.py
+++ PageBot2.py
@@ -1,210 +1,251 @@
 import praw
-import prawcore.exceptions as exceptions
-import re
-import math
-import time
-from datetime import datetime
-import sys, os
+from praw.exceptions import APIException
+from prawcore.exceptions import RequestException, NotFound, Forbidden
+from praw.models import Comment, Message
+
+import re, math, logging, time
+import sys, os, traceback
 
 class PageBot():
-    user_agent = "User-Agent:Parliament Pager:v2.3.0 (by /u/Zagorath)"
-    error_note = "No valid paging order was found in this message, sorry."
+	user_agent = "User-Agent:Parliament Pager:v2.3.0 (by /u/Zagorath)"
 
-    def __init__(self):
-        # Connect and log in to Reddit
-        self.r = praw.Reddit('PageBot', user_agent=self.user_agent)
-        #try:
-        #    self.o = OAuth2Util.OAuth2Util(self.r, print_log=False)
-        #    self.o.refresh(force=True)
-        #except Exception as e:
-        #    print e
+	def __init__(self):
+		self.r = praw.Reddit('PageBot', user_agent=self.user_agent)
+		logging.basicConfig(
+			filename='pagebot.log',
+			level=logging.WARNING,
+			format='%(asctime)s - %(levelname)s - %(message)s'
+		)
 
-        while True:
-            try:
-                self._run()
-                time.sleep(30)
-            except Exception as e:
-                # If there's an error that isn't caught,
-                # print the time and type of error
-                print datetime.now()
-                print e
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                print exc_type, exc_tb.tb_lineno
-                # Wait 15 minutes before trying again
-                time.sleep(15*60)
-                print "Starting again"
-                continue
+		while True:
+			try:
+				self.run()
+				time.sleep(30)
+			except Exception as e:
+				# If there's an error that isn't otherwise caught
+				# Make sure it is logged.
+				logging.critical(e)
+				exc_type, exc_obj, exc_tb = sys.exc_info()
+				msg = str(exc_type) + str(exc_tb.tb_lineno)
+				logging.critical(msg)
 
-    def _run(self):
-        for self.message in self.r.inbox.unread():
-            if "+/u/parliamentpagebot" in self.message.body.lower():
-                # pass to page
-                print "\nValid page order received"
-                self._page()
-            elif self.message.body.startswith('**gadzooks!'):
-                print "Received mod invite"
-                sr = self.message.subreddit # no idea if this works
-                #try:
-                sr.mod.accept_invite()
-                # Commenting out because unsure of how to do this in PRAW 4.
-                #except exceptions.InvalidInvite:
-                #    print "Mod invite accepting failed"
-                #    continue
-                self.r.inbox.mark_read(self.message)
+				# Wait 15 minutes before trying again
+				time.sleep(15*60)
+				logging.debug("Starting again")
+				continue
 
-            else:
-                print "Mentioned but no page order"
-                print self.message.body
-                self.r.inbox.mark_read(self.message)
+	def run(self):
+		"""Runs through all unread messages in the inbox
+		Passes valid mentions to the pager
+		Accepts mod invites
+		"""
+		for self.message in self.r.inbox.unread():
+			# Handle PMs. Mostly dealing with mod & approved submitter status
+			if isinstance(self.message, Message):
+				# If the message is a mod invitation, accept it
+				if self.message.body.startswith('**ggadzooks!'):
+					self.accept_mod_invite()
+				# If it's not a mod invite (mod acceptance, removal, approved submitter)
+				else:
+					logging.info("Message with id {} received titled {}".format(
+						self.message.id, self.message.subject))
+			# If the message is a comment
+			elif isinstance(self.message, Comment):
+				# If it mentions this bot and prefixes it with +
+				if "+/u/parliamentpagebot" in self.message.body.lower():
+					self._log(logging.INFO, "Valid page order received")
+					self.page()
+				# If it's a comment that mentions but doesn't prefix with +
+				elif "u/parliamentpagebot" in self.message.body.lower():
+					self._log(logging.INFO, "Bot mentioned, no page order")
+				# If it's a comment reply
+				else:
+					stop_messages = set(['stop', 'fuck', 'unsubscribe'])
+					stopped = False
+					# If indicates user wants to be removed from paging list contact mods.
+					for word in stop_messages:
+						if self.message.body.contains(word):
+							unsub = ("This bot replies to all users on the subreddit's "
+							      "paging list. Contact mods of the subreddit "
+							      "being paged if you wish to be removed from the list.")
+							self.reply(unsub)
+							stopped = True
+							self._log(logging.CRITICAL, "Request to stop replied to")
+					# If the comment has an unknown cause
+					if not stopped:
+						self._log(logging.INFO, "Unknown comment received")
+			self._log(logging.DEBUG, "Message dealt with")
+			self.message.mark_read()
+			self._log(logging.DEBUG, "Message marked unread")
 
-    def _page(self):
-        '''
-        From the current self.message, parse out the page order
-        (demarked by "+/u/ParliamentPageBot")
-        and page each of the users represented by the message
-        '''
-        page_orders = re.split("\+\/u\/parliamentpagebot", self.message.body, flags=re.IGNORECASE)
-        if len(page_orders) == 2:
-            self._parse_order(page_orders[1])
-        elif len(page_orders) > 2:
-            print "More than one page order received"
-            self.reply("More than one paging order found.")
-        else:
-            print "No page orders received"
-            # No valid paging orders were found
-            self.reply(self.error_note)
-        self.r.inbox.mark_read(self.message)
+	def page(self):
+		"""From the current self.message, parse out the page order
+		(demarked by "+/u/ParliamentPageBot")
+		and page each of the users represented by the message.
+		"""
+		self._log(logging.DEBUG, "Comment with body '{}' of type '{}'".format(
+			self.message.body, type(self.message.body)))
+		page_orders = re.split(
+			"\+\/u\/parliamentpagebot",
+			self.message.body,
+			flags=re.IGNORECASE
+		)
+		logging.debug("page_orders are '{}' of type '{}'".format(
+			page_orders, type(page_orders)))
+		# If exactly one page order is found
+		if len(page_orders) == 2:
+			self.parse_order(page_orders[1])
+		# If too many page orders are found
+		elif len(page_orders) > 2:
+			self.reply("More than one paging order found. "
+			          "Please only use one page order per comment. "
+			          "You can, however, page multiple subreddits' pagelists "
+			          "by including more than one in the command.")
+			self._log(logging.INFO, "More than one page order found")
+		# This code should never be reached
+		else:
+			self._log(
+				logging.ERROR,
+				"Paging syntax was used without valid order "
+			)
 
-    def _parse_order(self, page_order):
-        '''
-        Takes a page order (ideally a string representation of a list
-        of subreddits or users to page) and adds all valid entries
-        to the to_page list.
-        '''
-        self.reason = ''
-        self.to_page = set()
-        print "Page order:", page_order
-        try:
-            self.reason = page_order.split('[')[1].split(']')[0]
-            print "Reason:", self.reason
-        except IndexError as e:
-            print "No message"
-        # Remove the reason and any newlines from the command.
-        paging_commands = page_order.replace(self.reason, '')
-        paging_commands = paging_commands.replace('\n', ' ')
-        paging_commands = paging_commands.replace('\r', ' ')
-        here = set(["here", "this", "self"])
-        # "item" should be either a subreddit (prefixed with '/r/')
-        # or a user (prefixed with '/u/')
-        # or should be one of "here", "this", or "self"
-        print "Paging list (page order without reason):", paging_commands
-        paging_list = paging_commands.split(' ')
-        # Remove punctuation from commands
-        paging_list = [l.rstrip("'\",.:;?!#&*()@") for l in paging_list]
-        for item in paging_list:
-            print "Item to be paged:", item
-            if item.startswith("/r/") or item.startswith("r/"):
-              self._page_subreddit(item)
-##            elif item.startswith("/u/") or item.startswith("u/"):
-##                self.to_page.add(item)
-            elif item in here:
-              self._page_subreddit(self.message.subreddit)
-        print "Paging", self.to_page
-        self._do_page()
+	def parse_order(self, page_order):
+		"""Takes a page order and adds all valid entries to the to_page list
+		page_order is the string representing list of subreddits to page,
+		and an instruction to be delivered with the command (in square brackets)
+		"""
+		self.reason = ''
+		self.to_page = set()
+		logging.debug(
+			"Page order in {} is {}".format(self.message.id, page_order))
+		try:
+			self.reason = page_order.split('[')[1].split(']')[0]
+			logging.debug("Reason for {} is {}".format(self.message.id, self.reason))
+		except IndexError as e:
+			self._log(logging.INFO, "No message contained in comment")
+		# Remove the reason and any newlines from the command
+		paging_commands = page_order.replace(self.reason, '')
+		paging_commands = paging_commands.replace('\n', ' ')
+		paging_commands = paging_commands.replace('\r', ' ')
 
-    def _page_subreddit(self, subreddit):
-        '''
-        Adds users from the specified subreddit's "pagelist"
-        to the to_page list
-        '''
-        # should be unneccessary in PRAW 4 as subreddit is a Subreddit object.
-        #s = self.r.subreddit(subreddit.replace('r/', '', 1).replace('/', ''))
-        try:
-            wiki = subreddit.wiki["pagelist"]
-            contents = wiki.content_md.split("\r\n\r\n")
-            for user in contents:
-                self.to_page.add(user)
-        # If there is no pagelist page created
-        except exceptions.NotFound as e:
-            error_message = "No paging list found at %s/wiki/pagelist" % subreddit
-            err_msg = error_message.replace("//", "/")
-            self.message.reply(err_msg)
-        except exceptions.Forbidden as f:
-            error_message = "Sorry, I don't have permission to read the wiki page %s/wiki/pagelist. Make sure the bot has permission to read the wiki, possibly by adding it to the modlist with wiki permission." % subreddit
-            err_msg = error_message.replace("//", "/")
-            print err_msg
-            self.message.reply(err_msg)
+		here = set(['here', 'this', 'self'])
+		self._log(logging.DEBUG,
+			"Paging list (page order without reason): '{}'".format(paging_commands))
+		paging_list = paging_commands.split(' ')
+		logging.debug("Paging list: '{}'".format(paging_list))
+		# Remove punctuation from commands
+		paging_list = [l.rstrip("'\",.:;?!#&*()@") for l in paging_list]
+		# "item" should either be a subreddit (prefixed with /r/)
+		# or should be one of "here", "this", or "self"
+		for item in paging_list:
+			logging.debug("Item to be paged: {} of type {}".format(item, type(item)))
+			# if the item is a specified subreddit
+			if item.startswith("/r/") or item.startswith("r/"):
+				logging.debug("{} starts with /r/ or r/".format(item))
+				sub_name = item.replace('r/', '', 1).replace('/', '')
+				logging.debug("Subreddit's name is {}".format(sub_name))
+				s = self.r.subreddit(sub_name)
+				self.add_users(s)
+			# TODO: work out specific username paging, put it here
+			elif item.lower() in here:
+				logging.debug("The item is in the 'here' list")
+				self.add_users(self.message.subreddit)
+		logging.debug("Users being paged: ".format(self.to_page))
+		self.page_users()
+		logging.debug("All users have been paged")
 
-    def _do_page(self):
-        l = list(self.to_page)
-        num_replies = int(math.ceil(len(self.to_page)/3.0))
-        if num_replies < 1:
-            self.reply("Sorry, but no users to page were found")
-            #try:
-            #    self.message.reply("Sorry, but no users to page were found")
-            #except Exception as e:
-            #    print e
-            #    wait_ten()
-            #    self.message.reply("Sorry, but no users to page were found")
-        for i in range(num_replies):
-            # Page each of the members
-            if i*3 + 2 < len(self.to_page):
-                # If there are at least 3 members left to page
-                self.reply("Paging {0}, {1}, and {2} {3}".format(l[i*3], l[i*3 + 1], l[i*3 + 2], self.reason))
-                #try:
-                #    print "Paging {0}, {1}, and {2} {3}".format(l[i*3], l[i*3 + 1], l[i*3 + 2], self.reason)
-                #    self.message.reply("Paging {0}, {1}, and {2} {3}".format(l[i*3], l[i*3 + 1], l[i*3 + 2], self.reason))
-                #except Exception as e:
-                #    print e
-                #    wait_ten()
-                #    self.message.reply("Paging {0}, {1}, and {2} {3}".format(l[i*3], l[i*3 + 1], l[i*3 + 2], self.reason))
-            elif i*3 + 1 < len(self.to_page):
-                # If there are two more to be paged, page them
-                self.reply("Paging {0} and {1} {2}".format(l[i*3], l[i*3 + 1], self.reason))
-                #try:
-                #    print "Paging {0} and {1} {2}".format(l[i*3], l[i*3 + 1], self.reason)
-                #    self.message.reply("Paging {0} and {1} {2}".format(l[i*3], l[i*3 + 1], self.reason))
-                #except Exception as e:
-                #    print e
-                #    wait_ten()
-                #    self.message.reply("Paging {0} and {1} {2}".format(l[i*3], l[i*3 + 1], self.reason))
-            else:
-                # If only one member left to be paged, page them
-                self.reply("Paging {0} {1}".format(l[i*3], self.reason))
-                #try:
-                #    print "Paging {0} {1}".format(l[i*3], self.reason)
-                #    self.message.reply("Paging {0} {1}".format(l[i*3], self.reason))
-                #except Exception as e:
-                #    print e
-                #    wait_ten()
-                #    self.message.reply("Paging {0} {1}".format(l[i*3], self.reason))
 
-    def reply(self, message):
-        success = False
-        i = 0
-        print message
-        while not success:
-            try:
-                self.message.reply(message)
-                success = True
-            except Exception as e:
-                i += 1
-                if i > 5:
-                    return
-                print e
-                wait_ten()
+	def add_users(self, subreddit):
+		"""Adds users from the specified subreddit's "pagelist"
+		to the to_page list
+		"""
+		try:
+			wiki = subreddit.wiki['pagelist']
+			contents = wiki.content_md.split('\r\n\r\n')
+			logging.debug(
+				"Adding users to the to_page list from {}".format(subreddit))
+			for user in contents:
+				self.to_page.add(user)
+				if not isinstance(user, basestring):
+					logging.critical(
+						"Attempting to add '{}' to page list, not a str".format(user))
+		# If there is no pagelist page created
+		except NotFound as e:
+			error_message = "No paging list found at /r/{}/wiki/pagelist".format(
+				subreddit.display_name)
+			self._log(logging.INFO, "No wiki was found for {}".format(
+				subreddit.display_name))
+			self.reply(error_message)
+		except Forbidden as f:
+			error_message = ("Sorry, I don't have permission to read the wiki page "
+			             "/r/{}/wiki/pagelist. Make sure the bot has permission to "
+			             "read the wiki, either by adding it to the modlist with "
+			             "wiki permission, or by making the wiki public.".format(
+				subreddit.display_name))
+			self._log(logging.INFO, "Was unable to view /r/{} wiki".format(
+				subreddit.display_name))
+			self.reply(error_message)
 
-def wait_ten():
-    '''
-    Wait 10 minutes, printing each minute
-    '''
-    for i in range(10):
-        print "Trying again in %i minutes" % (10 - i)
-        time.sleep(60)
-    print "Trying again now"
-    time.sleep(2)
+	def page_users(self):
+		l = list(self.to_page)
+		num_replies = int(math.ceil(len(self.to_page)/3.0))
+		if num_replies < 1:
+			self._log(logging.WARNING, "No users were found to page")
+			self.reply("Sorry, but no users to page were found")
+		for i in range(num_replies):
+			# Page each of the members
+			if i*3 + 2 < len(self.to_page):
+				# If there are at least 3 members left to page, page the next three
+				self.reply("Paging {0}, {1}, and {2} {3}".format(
+					l[i*3], l[i*3 + 1], l[i*3 + 2], self.reason))
+			elif i*3 + 1 < len(self.to_page):
+				# If there are two more to be paged, page them
+				self.reply("Paging {0} and {1} {2}".format(
+					l[i*3], l[i*3 + 1], self.reason))
+			else:
+				# If only one member left to be paged, page them
+				self.reply("Paging {0} {1}".format(l[i*3], self.reason))
 
-#######################################################
+
+	def reply(self, message):
+		"""Replies to self.message with the given message
+		If it to connect, it waits 1 minute before trying again,
+		doubling the time interval until it works.
+		"""
+		success = False
+		minutes = 1
+		while not success:
+			try:
+				self.message.reply(message)
+				success = True
+			except RequestException as e:
+				logging.warning("Failed to connect or other RequestException.")
+				time.sleep(minutes * 60)
+				minutes *= 2
+				if minutes > 32:
+					self._log(
+						logging.CRITICAL,
+						"Have tried for over an hour to reply to message"
+					)
+					return
+
+	def accept_mod_invite(self):
+		logging.info("Received mod invite to {}".format(self.message.subreddit))
+		sr = self.message.subreddit
+		try:
+			sr.mod.accept_invite()
+		except APIException as e:
+			logging.warning(
+				"APIException raised in attempting to accept mod invite."
+			)
+
+	def _log(self, level, message):
+		message += " -- by {} with id {}".format(
+			self.message.author,
+			self.message.id)
+		logging.log(level, message)
+
+##############################################################
 
 if __name__ == '__main__':
-    PageBot()
+	PageBot()
